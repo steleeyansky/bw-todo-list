29ed4c7b7f6527ff43647fec4ab489bc22da132e
Slyubenov <nobody@example.com>
2023-12-20 18:39:38 +0200

    Remove vendor from gitignoe

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'some-bw-todo-vendor-name-here/bw-todo-list',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '472be1b5f60b362bc07554a8e392f273b065b384',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'htmlburger/carbon-fields' => array(
+            'pretty_version' => 'v3.6.3',
+            'version' => '3.6.3.0',
+            'reference' => 'd913a5148cb9dc61ed239719c747f4ebb513003f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../htmlburger/carbon-fields',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'some-bw-todo-vendor-name-here/bw-todo-list' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '472be1b5f60b362bc07554a8e392f273b065b384',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
